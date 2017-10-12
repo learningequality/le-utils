ba9acbe8561f7271d203f03d90cba08b79092805
Ivan Savov <ivan.savov@gmail.com>
2017-10-12 13:58:06 -0700

    Implemented getlang_by_native_name for easier lookups (#31)

--- tests/test_getlangs.py
+++ tests/test_getlangs.py
@@ -110,6 +110,7 @@ def test_list_like_language_names():
 # getlang_by_alpha2 ==> Lookup by two-letter Language code
 ################################################################################
 
+
 def test_known_alpha2_codes():
     lang_obj = languages.getlang_by_alpha2('en')
     assert lang_obj is not None, 'English not found'
@@ -132,5 +133,127 @@ def test_known_alpha2_codes():
 def test_unknown_alpha2_code():
     lang_obj = languages.getlang_by_alpha2('zz')
     assert lang_obj is None, 'Uknown code zz returned non-None'
+
+
+
+
+# getlang_by_native_name ==> Lookup by Language object by native_name
+################################################################################
+
+def test_known_native_names():
+    lang_obj = languages.getlang_by_native_name('English')
+    assert lang_obj is not None, 'English not found'
+    assert lang_obj.code == "en", 'Wrong code'
+    assert lang_obj.name == "English", 'Wrong name'
+    assert lang_obj.native_name == "English", 'Wrong native_name'
+
+    lang_obj = languages.getlang_by_native_name('isiZulu')
+    assert lang_obj is not None, 'Zulu not found'
+    assert lang_obj.code == "zul", 'Wrong internal repr. code'
+    assert lang_obj.name == "Zulu", 'Wrong name'
+    assert lang_obj.native_name == "isiZulu", 'Wrong native_name'
+
+    # NOTE: Currently only support full-name matching so would have to lookup by
+    #       "name, country" to get local language version
+    lang_obj = languages.getlang_by_native_name('Português')
+    assert lang_obj is not None, 'Brazilian Portuguese not found'
+    assert lang_obj.code == "pt", 'Wrong internal repr. code'
+    assert lang_obj.name == "Portuguese", 'Wrong name'
+    assert lang_obj.native_name == "Português", 'Wrong native_name'
+
+    # NOTE: Currently only support full match lookups where multiple language
+    #       specified spearated by semicolons, e.g. "Scottish Gaelic; Gaelic"
+    lang_obj = languages.getlang_by_native_name('Gàidhlig')
+    assert lang_obj is not None, 'Scottish Gaelic; Gaelic not found'
+    assert lang_obj.code == "gd", 'Wrong internal repr. code'
+    assert lang_obj.name == "Scottish Gaelic; Gaelic", 'Wrong name'
+    assert lang_obj.native_name == "Gàidhlig", 'Wrong native_name'
+
+
+def test_unknown_name():
+    lang_obj = languages.getlang_by_native_name('UnknoenNativeLanguage')
+    assert lang_obj is None, 'query for natove_name UnknoenNativeLanguage returned non-None'
+
+def test_language_names_with_modifier_in_bracket():
+    # try to match based on language name (stuff before subcode in brackets)
+    lang_obj = languages.getlang_by_native_name('中文')
+    assert lang_obj is not None, 'Chinese 1 not found'
+    assert lang_obj.code == "zh", 'Wrong internal repr. code'
+    assert lang_obj.name == "Chinese", 'Wrong name'
+    assert lang_obj.native_name == "中文 (Zhōngwén), 汉语, 漢語", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('汉语')
+    assert lang_obj is not None, 'Chinese 2 not found'
+    assert lang_obj.code == "zh", 'Wrong internal repr. code'
+    assert lang_obj.name == "Chinese", 'Wrong name'
+    assert lang_obj.native_name == "中文 (Zhōngwén), 汉语, 漢語", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('漢語')
+    assert lang_obj is not None, 'Chinese 3 not found'
+    assert lang_obj.code == "zh", 'Wrong internal repr. code'
+    assert lang_obj.name == "Chinese", 'Wrong name'
+    assert lang_obj.native_name == "中文 (Zhōngwén), 汉语, 漢語", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('日本語')
+    assert lang_obj is not None, 'Japanese not found'
+    assert lang_obj.code == "ja", 'Wrong internal repr. code'
+    assert lang_obj.name == "Japanese", 'Wrong name'
+    assert lang_obj.native_name == "日本語 (にほんご／にっぽんご)", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('한국어')
+    assert lang_obj is not None, 'Korean not found'
+    assert lang_obj.code == "ko", 'Wrong internal repr. code'
+    assert lang_obj.name == "Korean", 'Wrong name'
+    assert lang_obj.native_name == "한국어 (韓國語), 조선말 (朝鮮語)", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('조선말')
+    assert lang_obj is not None, 'Korean not found'
+    assert lang_obj.code == "ko", 'Wrong internal repr. code'
+    assert lang_obj.name == "Korean", 'Wrong name'
+    assert lang_obj.native_name == "한국어 (韓國語), 조선말 (朝鮮語)", 'Wrong native_name'
+
+
+def test_list_like_language_native_names():
+    lang_obj = languages.getlang_by_native_name('Iñupiaq')
+    assert lang_obj is not None, 'Inupiaq not found'
+    assert lang_obj.code == "ik", 'Wrong internal repr. code'
+    assert lang_obj.name == "Inupiaq", 'Wrong name'
+    assert lang_obj.native_name == "Iñupiaq, Iñupiatun", 'Wrong native_name'
+    #
+    lang_obj = languages.getlang_by_native_name('Iñupiatun')
+    assert lang_obj is not None, 'Inupiaq not found'
+    assert lang_obj.code == "ik", 'Wrong internal repr. code'
+    assert lang_obj.name == "Inupiaq", 'Wrong name'
+    assert lang_obj.native_name == "Iñupiaq, Iñupiatun", 'Wrong native_name'
+
+
+
+@pytest.fixture
+def african_languages_list():
+    return ['Sesotho', 'isiXhosa', 'isiZulu', 'isiNdebele', 'Setswana', 'Siswati', 'Xitsonga']
+
+def test_african_languages(african_languages_list):
+    missing_names = []
+    for native_name in african_languages_list:
+        lang_obj = languages.getlang_by_native_name(native_name)
+        if lang_obj is None:
+            missing_names.append(native_name)
+    assert missing_names == [], 'Languages with native_names missing: ' + str(missing_names)
+
+
+
+@pytest.fixture
+def african_languages_list2():
+    return ['Sepedi', 'Tshivenda']
+
+@pytest.mark.skip('These languages are not in json data yet')
+def test_african_languages2(african_languages_list2):
+    missing_names = []
+    for native_name in african_languages_list2:
+        lang_obj = languages.getlang_by_native_name(native_name)
+        if lang_obj is None:
+            missing_names.append(native_name)
+    print('missing_names=', missing_names)
+    assert missing_names == [], 'Languages with native_names missing: ' + str(missing_names)
 
 
