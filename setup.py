b263d33c2c8ad4132ac3ca0fb4b458cc3ce924b3
Blaine Jester <blainesworld@gmail.com>
2019-06-07 13:19:31 -0700

    Bump version and update changelog

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 setup(
     name="le-utils",
     packages = find_packages(),
-    version="0.1.18",
+    version="0.1.19",
     description="LE Utils and constants shared across Kolibri, Ricecooker, and the Kolibri Studio.",
     long_description=long_description,
     install_requires=requirements,
