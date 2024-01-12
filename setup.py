b17514f7634a478e5aa0ec64dd1c10737030088e
Richard Tibbles <richard@learningequality.org>
2024-01-12 08:03:34 -0800

    Expand Python support for le-utils for ease.

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setup(
     ],
     author="Learning Equality",
     author_email="info@learningequality.org",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*,  <3.12",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*,  <3.13",
 )
